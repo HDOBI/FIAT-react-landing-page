e5d8885d2b11085eae0454326193413dcdc3e9c6
hassan <hassanbajwa154@gmail.com>
2024-11-17 03:39:11 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import AudioPlayer from "./components/AudioPlayer";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Mixtape from "./components/Mixtape";
 
 function App() {
   return (
-   <>
-   <Header/>
-    <Mixtape/>
-   </>
+    <>
+      <Header />
+      <Home/>
+      <Mixtape />
+      <AudioPlayer />
+    </>
   );
 }
 
---
+++ src/components/AudioPlayer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AudioPlayer() {
+  return (
+    <div className="min-w-full">
+      <iframe
+        width="auto"
+        className="min-w-full"
+        height="500"
+        scrolling="no"
+        frameborder="no"
+        src="https://fidbak.audio/fyebabykrzy/embed/84c27b2abdfb/05f028bbd574"
+      ></iframe>
+    </div>
+  );
+}
+
+export default AudioPlayer;
