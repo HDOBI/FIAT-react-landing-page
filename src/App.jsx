c2fd92a8ea7805e8f62986f685c9eba6121134f2
hassan <hassanbajwa154@gmail.com>
2024-11-18 01:41:27 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Bottom from "./components/Bottom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Mixtape from "./components/Mixtape";
+import MusicIcons from "./components/MusicIcons";
 import Socialicons from "./components/Socialicons";
 import WhereTo from "./components/WhereTo";
 
 function App() {
   return (
-    <div class='max-w-[1200px] mx-auto overflow-x-hidden'>
+    <div class="max-w-[1200px] mx-auto overflow-x-hidden">
       <Header />
-      <Home/>
+      <Socialicons/>
+      <Home />
       <Mixtape />
       <AudioPlayer />
-      <Socialicons/>
-      <BlessingCover/>
-      <WhereTo/>
-      <Bottom/>
+      <MusicIcons />
+      <BlessingCover />
+      <WhereTo />
+      <Bottom />
     </div>
   );
 }
--- src/components/MusicIcons.jsx
+++ src/components/MusicIcons.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import socialData from "../utility/socialicons.json";
+import musicIconsData from "../utility/musicIcons.json";
 
-function Socialicons() {
-
-    console.log(socialData.icon)
+function MusicIcons() {
   return (
     <div className="flex flex-col gap-[45px] sm:gap-0 md:flex-row items-center justify-between max-w-[1200px] mx-auto py-[35px] sm:py-[60px] px-2 sm:px-5">
-      {socialData.map((iconsList, index) => (
+      {musicIconsData.map((iconsList, index) => (
         <img
           key={index}
           src={iconsList.icon}
@@ -19,4 +17,4 @@ function Socialicons() {
   );
 }
 
-export default Socialicons;
+export default MusicIcons;
