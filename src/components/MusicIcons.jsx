f98f1acb490d4cef9f69a7369abba134b31d8841
hassan <hassanbajwa154@gmail.com>
2024-11-19 14:57:01 +0500

    update

--- src/components/MusicIcons.jsx
+++ src/components/MusicIcons.jsx
@@ -3,15 +3,19 @@ import musicIconsData from "../utility/musicIcons.json";
 
 function MusicIcons() {
   return (
-    <div className="flex flex-col gap-[45px] sm:gap-0 md:flex-row items-center justify-between max-w-[1200px] mx-auto py-[35px] sm:py-[60px] px-2 sm:px-5">
+    <div className="flex flex-row items-center justify-between max-w-[1200px] mx-auto py-[35px] sm:py-[60px] px-2 sm:px-5">
       {musicIconsData.map((iconsList, index) => (
+        <a href={iconsList.link} target="_blank">
         <img
           key={index}
           src={iconsList.icon}
           className={`w-auto ${
-            iconsList.icon === "/icons/brandcamp.png" ? "max-h-[85px] md:max-h-[60px] pt-[25px] md:pt-0" : "max-h-[140px]"
+            iconsList.icon === "/icons/brandcamp.png"
+              ? "max-h-[23px] md:max-h-[60px]"
+              : "max-h-[50px] md:max-h-[140px]"
           }`}
         />
+        </a>
       ))}
     </div>
   );
--- src/components/Socialicons.jsx
+++ src/components/Socialicons.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import socialData from "../utility/socialicons.json";
+import socialIconsData from "../utility/socialicons.json";
 
 function Socialicons() {
   return (
-    <div className="flex flex-col gap-[45px] sm:gap-0 md:flex-row items-center justify-between max-w-[1200px] mx-auto py-[35px] sm:py-[60px] px-2 sm:px-5">
-      {socialData.map((iconsList, index) => (
+    <div className="flex flex-row items-center justify-between max-w-[1200px] mx-auto py-[35px] sm:py-[60px] px-2 sm:px-5">
+      {socialIconsData.map((iconsList, index) => (
+        <a href={iconsList.link} target="_blank">
         <img
           key={index}
           src={iconsList.icon}
           className={`w-auto ${
             iconsList.icon === "/icons/brandcamp.png"
-              ? "max-h-[85px] md:max-h-[60px] pt-[25px] md:pt-0"
-              : "max-h-[140px]"
+              ? "max-h-[23px] md:max-h-[60px]"
+              : "max-h-[50px] md:max-h-[140px]"
           }`}
         />
+        </a>
       ))}
     </div>
   );
