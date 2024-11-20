182d695c802fce089db4a28f411afe36225fab58
hassan <hassanbajwa154@gmail.com>
2024-11-21 00:01:05 +0500

    update

--- src/components/Shoutout.jsx
+++ src/components/Shoutout.jsx
@@ -8,10 +8,10 @@ function Shoutout() {
   const [showSvg, setShowSvg] = useState(false)
   
   return (
-    <div className="relative" onClick={() => setShowSvg(true)}>
+    <div className="relative" onClick={() => setShowSvg(!showSvg)}>
       <img src={coverimg} className="" />
       <div class="absolute top-0 left-0 right-0 bottom-0 flex justify-center text-center items-center">
-        <div  className={`relative cursor-pointer transition-opacity ease-in-out duration-1000 ${showSvg ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} >
+        <div  className={`relative cursor-pointer transition-opacity ease-in-out duration-1000 ${showSvg ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
         <img src={drawingGif} className="max-w-[320px] xs:max-w-[400px] sm:max-w-[800px]" />
         <div className="absolute top-0 left-0 right-0 bottom-0 xs:px-5 uppercase text-center flex flex-col justify-between">
         <h1 className="text-[#FF0707] text-shadow font-bold text-[14px] leading-[15px] xs:text-[16px] xs:leading-[17px] sm:text-[25px] sm:leading-[26px]">SHoutout to ALL the producers who provided inspiration for this tape //</h1>
